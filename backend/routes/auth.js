fe9297d7f52e6841b114f44f4f8c7b084bef699c
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-03-09 15:56:44 +0530

    Update backend/routes/auth.js file -- code of existing user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 // const fetchuser = require("../middleware/fetchUser");
-require('dotenv').config();
+require("dotenv").config();
 
 const JWT_secret = "somethingtoknow";
 
@@ -14,8 +14,8 @@ router.post(
   "/newuser",
   [
     // CHECK 1 : Entered credentials are checking by the validation function.
-    body("first_name", "Enter a valid name").isLength({max : 50}),
-    body("last_name", "Enter a  valid last name").isLength({max: 50}),
+    body("first_name", "Enter a valid name").isLength({ max: 50 }),
+    body("last_name", "Enter a  valid last name").isLength({ max: 50 }),
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password must be of atleast 6 characters").isLength({
       min: 6,
@@ -31,7 +31,7 @@ router.post(
       // CHECK 2 : dont want two or more user of same email id.
       let newUser = await user.findOne({ email: req.body.email });
       if (newUser) {
-        return res.status(400).json({message : "User already exists"});
+        return res.status(400).json({ message: "User already exists" });
       }
       // salt and hash we are using to ensure better security to the user.
       // gensalt() function creates a unique set of number or letters which add-up to the actual password
@@ -41,8 +41,8 @@ router.post(
       const secPass = await bcrypt.hash(req.body.password, salt);
       // DOCUMENT CREATION : User entered data is being send to the database.
       newUser = await user.create({
-        first_name : req.body.first_name,
-        last_name : req.body.last_name,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
         email: req.body.email,
         password: secPass,
       });
@@ -53,7 +53,7 @@ router.post(
         },
       };
       const auth_token = jwt.sign(data, JWT_secret);
-      res.json({auth_token });
+      res.json({ auth_token });
     } catch (error) {
       // throw errors.
       console.error(error.message);
@@ -62,52 +62,47 @@ router.post(
   }
 );
 
-
 // ROUTE 2 : Authenticate a user using : POST /api/auth/login . login required.
 router.post(
-  "/login",[
+  "/login",
+  [
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password cannot be blank").exists(),
   ],
   async (req, res) => {
-    let outcome = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({outcome, errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
-    // EXTRACTION : extracting pass and email to verify it with the existing mongodb database.
+
     const { email, password } = req.body;
     try {
       // CHECK 1 : Email
-      // user.findOne({ email }), it queries the MongoDB database to find a document where the "email" field matches the provided email. If there's a match, it retrieves the entire document associated with that email
+      // matching will make entire document avaiable to use.
       let existingUser = await user.findOne({ email });
       if (!existingUser) {
-        return res.status(401).json({outcome, msg: "Invalid Credentials" });
+        return res.status(401).json({ outcome, msg: "Invalid Credentials" });
       }
-      // CHECK 2 : Password
-      // entered password and stored password should match to verify user
+      // CHECK 2 : Password (comparision by using bcrypt library).
+
       const existingPassword = await bcrypt.compare(
         password,
         existingUser.password
       );
       if (!existingPassword) {
-        return res.status(401).json({outcome, msg: "Invalid Credentials" });
+        return res.status(401).json({ outcome, msg: "Invalid Credentials" });
       }
-      // If everything is fine then generate token for that particular user & send it in json format
+      // Storing current user document id to use it with the jwt token.
       const data = {
         newUser: {
           id: existingUser.id,
         },
       };
-      // HERE : we creating a token by using the user id (stored in data-base) and jwt-secret-key which only backend knows
+      // Token creation.
       const auth_token = jwt.sign(data, JWT_secret);
-      console.log(auth_token);
-      if(auth_token){
-        outcome = true
-      }
-      // Sending token to the client
-      res.json({outcome, auth_token });
+      res.json({ auth_token });
     } catch (error) {
+      // for errors.
       console.error(error.message);
       res.status(500).send("Server Error Occured");
     }
