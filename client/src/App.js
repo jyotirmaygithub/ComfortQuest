937b082ebd701b2bace750d23b7b354abe9fcebb
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-03-19 18:23:34 +0530

    setting of auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Auth0Provider } from '@auth0/auth0-react';
 import Header  from "./components/header";
 
 function App() {
   return (
    <>
-   <Header/>
+   <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN_AUTH}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
+    <Header/>
+    </Auth0Provider>
    </>
   );
 }
