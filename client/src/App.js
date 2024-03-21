ac73b65166297eb86b14d53da17d957346bf06d6
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-03-21 15:55:44 +0530

    setting up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Auth0Provider } from '@auth0/auth0-react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header  from "./components/header";
+import LoginPage from "./pages/Login";
 
 function App() {
+  console.log("value of the client id = " , process.env.REACT_APP_AUTH_CLIENT_ID)
   return (
    <>
-   <Auth0Provider
-    domain={process.env.REACT_APP_DOMAIN_AUTH}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-    >
-    <Header/>
-    </Auth0Provider>
+   <GoogleOAuthProvider clientId={process.env.REACT_APP_AUTH_CLIENT_ID}>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Header />}/>
+        <Route exact path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </Router>
+    </GoogleOAuthProvider>;
    </>
   );
 }
