58947c9a723d2f6bc918848d39afae3b9a2724b2
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-03-22 10:55:48 +0530

    new route of not found

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header  from "./components/header";
 import LoginPage from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import NotFoundPage from "./pages/NotFound";
 
 function App() {
   console.log("value of the client id = " , process.env.REACT_APP_AUTH_CLIENT_ID)
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" element={<Header />}/>
         <Route exact path="/login" element={<LoginPage/>}/>
         <Route exact path="/signup" element={<SignUp/>}/>
+        <Route exact path="*" element={<NotFoundPage/>}/>
       </Routes>
     </Router>
     </GoogleOAuthProvider>;
