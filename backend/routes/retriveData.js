c2959ab3398cd18165ccbe20a37a758511394aa4
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-04-02 08:25:39 +0530

    backend/Route : to retrive single hotel data

--- backend/routes/retriveData.js
+++ backend/routes/retriveData.js
@@ -2,6 +2,23 @@ const Hotel = require("../models/Hotels");
 const express = require("express");
 const router = express.Router();
 
+router.post(
+  "/single-hotel",
+  async (req, res) => {
+    try {
+        const {id} = req.body;
+        const singleHotelData = await Hotel.findById({_id: id});
+        console.log("let see the hotel =", singleHotelData);
+        res.json({ HotelData: singleHotelData });
+    } catch (error) {
+      // throw errors.
+      console.error(error.message);
+      res.status(500).send("Internal server Error Occurred");
+    }
+  }
+);
+
+
 router.get("/hotels-data", async (req, res) => {
   try {
      // Retrieve all documents from the hotels collection
