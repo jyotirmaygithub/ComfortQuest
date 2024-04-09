995987bb8a3e9ab5d8aad8e892b6261b5e45a560
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-04-09 18:24:54 +0530

    layout to show booked hotels

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { HotelContext } from "../context/HotelsContext";
+import BookedLayout from "../Layout/BookedLayout";
 
-export default function Booking() {
+export default function BookingsPage() {
+  const { userBooking } = HotelContext();
+  const { userBookedData } = userBooking;
+
   return (
-    <div>
-      
+    <div className="container">
+      {userBookedData ? userBookedData.map((bookingData) => {
+        return <BookedLayout BookingData={bookingData} />;
+      }) : ""}
     </div>
-  )
+  );
 }
