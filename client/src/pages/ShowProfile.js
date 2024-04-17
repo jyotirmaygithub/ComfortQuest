b5c4af8b4bb96c0439312d89b130f510cb5cd15d
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-04-17 16:41:19 +0530

    popover to visible on show profile page

--- client/src/pages/ShowProfile.js
+++ client/src/pages/ShowProfile.js
@@ -4,6 +4,7 @@ import Avatar from "@mui/material/Avatar";
 import { StateContext } from "../context/States";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import Trial from "../components/PopUps/trial";
 
 export default function ActionAreaCard() {
   const { userDocument } = StateContext();
@@ -11,22 +12,31 @@ export default function ActionAreaCard() {
 
   return (
     <div className="flex justify-center items-center h-[100vh] w-[100vw] space-y-2">
-      <div className="flex-col justify-center border-2 border-black border-solid p-4 rounded-md">
+      <div className="flex-col justify-center border-2 border-stone-400 border-solid p-4 rounded-md space-y-10">
+        <Trial />
         <Avatar
           alt="profile picture"
           src={picture}
           sx={{ width: 250, height: 250 }}
         />
         <div className="space-y-2">
-          <div className="flex justify-start items-center space-x-2">
+          <div className="flex justify-start items-center space-x-4">
             <AccountCircleOutlinedIcon className="h-10 w-10" />
-            <Typography gutterBottom variant="h6" component="div">
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
               {name ? name : ""}
             </Typography>
           </div>
-          <div className="flex justify-start items-center">
+          <div className="flex justify-start items-center space-x-4">
             <EmailOutlinedIcon />
-            <Typography gutterBottom variant="h6" component="div">
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
               {email ? email : ""}
             </Typography>
           </div>
