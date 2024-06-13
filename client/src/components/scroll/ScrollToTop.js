fdd3a07e81a5bc49c8bae4a954a30ab4c559c6e2
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-06-13 18:50:28 +0530

    Add : scroll to top

---
+++ client/src/components/scroll/ScrollToTop.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import './scroll.css'; // Import your CSS file for styling (if needed)
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled up to certain point
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Scroll to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  // Add scroll event listener using useEffect
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className={`scroll-to-top ${isVisible ? 'visible' : ''}`}>
+      {isVisible && (
+        <Fab color="primary" onClick={scrollToTop} aria-label="scroll-to-top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTopButton;
