284c74cbe60615246ff7be7e7e18a1e6992432f7
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-06-20 18:45:48 +0530

    Feat : search field only on landing page

--- client/src/Layout/Header.js
+++ client/src/Layout/Header.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Logo from "../components/Header/Logo";
-import SearchBar from "../components/Header/SearchBar";
-import Avatar from "../components/Header/Avatar";
-import { useNavigate } from "react-router-dom";
+import SearchBar from "../components/Header/Search";
+import Avatar from "../components/Header/Toggle";
+import { useLocation } from 'react-router-dom';
 
 export default function HeaderLayout() {
-  const navigate = useNavigate();
-  function handleClick() {
-    navigate("/login");
-  }
+  const location = useLocation();
+  const {pathname} = location
+
   return (
     <header
       className={`fixed top-0 z-10 flex w-screen justify-center bg-white py-4 `}
     >
-      <div className={`flex w-screen max-w-screen-xl justify-evenly items-center`}>
+      <div className={`flex w-screen max-w-screen-xl justify-between items-center`}>
         <Logo />
-        <SearchBar />
+       {pathname === "/" &&  <SearchBar />}
         <Avatar />
       </div>
     </header>
